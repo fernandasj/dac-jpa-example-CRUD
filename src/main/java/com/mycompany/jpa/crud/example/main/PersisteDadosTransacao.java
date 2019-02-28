1dc305566a85e7e1f304080592f1d5caa2b77e92
fernandasj <fernanda.silva_sj@hotmailcom>
2019-02-27 21:58:06 -0300

    some fix

--- src/main/java/com/mycompany/jpa/crud/example/main/PersisteDadosTransacao.java
+++ src/main/java/com/mycompany/jpa/crud/example/main/PersisteDadosTransacao.java
@@ -2,22 +2,23 @@ package com.mycompany.jpa.crud.example.main;
 
 import com.mycompany.jpa.crud.example.domain.Pessoa;
 import javax.persistence.EntityManager;
-
+import javax.persistence.PersistenceContext;
 /**
  *
  * @author fernanda
  */
-public class PersisteDados {
+public class PersisteDadosTransacao {
     //instanciando o gerenciador de instancias
+    @PersistenceContext(unitName = "crud-jpa")
     private final EntityManager em;
 
-    public PersisteDados(EntityManager em) {
+    public PersisteDadosTransacao(EntityManager em) {
         this.em = em;
     }
     
     public void inserirDados(){
-        Pessoa p1 = new Pessoa("Fernanda");
-        Pessoa p2 = new Pessoa("Fer");
+        Pessoa p1 = new Pessoa("Ada Lovelace");
+        Pessoa p2 = new Pessoa("Dandara");
         
         //abrindo uma transaçao com o banco
         em.getTransaction().begin();
@@ -25,6 +26,9 @@ public class PersisteDados {
         //persistindo os dados no banco
         em.persist(p1);
         em.persist(p2);
+        
+        //sincronizando os dados
+        em.flush();
         
         //confirmando a persistencia dos dados no banco
         em.getTransaction().commit();
